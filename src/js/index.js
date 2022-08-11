250ef6497c3fadf80c6b0cce92d5814ca36fdaca
SamLinux <oxedom@gmail.com>
2022-08-11 17:07:26 +0300

    removed bootstrap from nodemoudles, replaced it to CDN to improve rendering time

--- src/js/index.js
+++ src/js/index.js
@@ -1,10 +1,6 @@
-import '../scss/styles.scss'
-// Import all of Bootstrap's JS
-import * as bootstrap from 'bootstrap'
+
+
 import './dom'
 
 console.log('amit');
 
-// function _getID() {
-//     return '_' + Math.random().toString(36).substr(2, 9);
-// }
--- webpack.config.js
+++ webpack.config.js
@@ -3,47 +3,13 @@ const path = require('path')
 module.exports = {
     entry: {
         index: './src/js/index.js',
-        another: './src/js/dom.js',
     },
     output: {
-        filename: '[name].bundle.js',
-        // filename: 'main.js',
+
+        filename: 'main.js',
         path: path.resolve(__dirname, 'dist')
     },
-    optimization: {
-        runtimeChunk: 'single',
-    },
-    devServer: {
-        static: path.resolve(__dirname, 'dist'),
-        port: 8888,
-        hot: true
-    },
     module: {
-        rules: [
-            {
-                test: /\.(scss)$/,
-                use: [
-                    {
-                        loader: 'style-loader'
-                    },
-                    {
-                        loader: 'css-loader'
-                    },
-                    {
-                        loader: 'postcss-loader',
-                        options: {
-                            postcssOptions: {
-                                plugins: () => [
-                                    require('autoprefixer')
-                                ]
-                            }
-                        }
-                    },
-                    {
-                        loader: 'sass-loader'
-                    }
-                ]
-            }
-        ]
+
     }
 }
